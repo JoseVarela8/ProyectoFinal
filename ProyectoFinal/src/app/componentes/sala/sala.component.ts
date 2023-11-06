eb494ad02b7fce9fb4bf75d1d434ce4b4619c1b2
NicolasMazzey <nicolasmazzey@gmail.com>
2023-11-06 18:29:55 -0300

    puntuar actidades

--- ProyectoFinal/src/app/componentes/sala/sala.component.ts
+++ ProyectoFinal/src/app/componentes/sala/sala.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { range } from 'rxjs';
+import { Action } from 'rxjs/internal/scheduler/Action';
+import { Actividad } from 'src/app/clases/actividad';
 import { Juego } from 'src/app/clases/juego';
 import { ControladorJuegosService } from 'src/app/servicios/controlador-juegos.service';
 
@@ -13,7 +16,10 @@ export class SalaComponent implements OnInit{
   enSala:boolean = false
   SalasActivas : Juego[] = []
   SalaActiva: Juego = new Juego
-
+  Actividades : Actividad[] = []
+  empezo: boolean = true
+  contador = 0;
+  Puntaje: number[] = []
 
   constructor(private controlador:ControladorJuegosService, private router: Router, private route:ActivatedRoute){}
 
@@ -29,7 +35,28 @@ export class SalaComponent implements OnInit{
       let variable = this.controlador.getJuego2(this.linksala)
       if (variable != null){
         this.SalaActiva = variable
+        if (this.SalaActiva.propuesta?.actividades != null){
+          this.Actividades = this.SalaActiva.propuesta?.actividades
+          let largo = this.Actividades.length
+          for (let i = 0; i < largo; i++) {
+            this.Puntaje.push(0)
+          }
+        }
       }
     }
+  }
+
+  votarMegusta(){
+    this.Puntaje[this.contador] = this.Puntaje[this.contador] + 1
+    this.contador = this.contador + 1
+  }
+
+  votarMedaigual(){
+    this.contador = this.contador + 1
+  }
+
+  votarNoMegusta(){
+    this.Puntaje[this.contador] = this.Puntaje[this.contador] - 1
+    this.contador = this.contador + 1
   }
 }
